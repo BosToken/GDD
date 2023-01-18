3885c57dcacd7263c8b6e698a3deec1deaa03720
Slavouz Corp <tmbwpva@protonmail.com>
2023-01-18 21:40:22 +0700

    Bug Fixes

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    KAKI player;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.Find("KAKI").GetComponent<KAKI>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position -= new Vector3 (9f*Time.deltaTime, 0, 0);
+        // transform.position -= new Vector3 (9f*Time.deltaTime, 0, 0);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 pos = transform.position;
+        pos.x -= player.velocity.x * Time.fixedDeltaTime;
+        if (pos.x < -20)
+        {
+            pos.x = 13;
+        }
+        transform.position = pos;
     }
 }
--- Assets/Scripts/KAKI.cs
+++ Assets/Scripts/KAKI.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class KAKI : MonoBehaviour
 {
+    KAKI player;
     public float gravity = -100;
     public Vector2 velocity;
     public float maxAcceleration = 10;
@@ -11,16 +13,17 @@ public class KAKI : MonoBehaviour
     public float distance = 0;
     public float jumpVelocity = 20;
     public float maxVelocity = 100;
-    public float groundHeight = 8;
+    public float groundHeight = 2;
     public bool isGrounded = false;
     public bool isHoldingJump = false;
     public float maxHoldJumpTime = 0.15f;
     public float holdJumpTimer = 0.0f;
     public float jumpGroundThreshold = 1;
+    public int health = 2;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.Find("KAKI").GetComponent<KAKI>();
     }
 
     // Update is called once per frame
@@ -42,6 +45,24 @@ public class KAKI : MonoBehaviour
         if (Input.GetKeyUp(KeyCode.Space))
         {
             isHoldingJump = false;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D Coll)
+    {
+        if(Coll.gameObject.tag=="Respawn")
+        {
+            health--;            
+            if (health == 1) {
+                Vector2 pos = transform.position;
+                pos.x -= player.velocity.x * Time.fixedDeltaTime;
+                pos.x = 13;
+                acceleration = 1;                             
+            }
+            else if (health == 0) {
+                SceneManager.LoadScene("Game");
+                //Game Over
+            }            
         }
     }
 
