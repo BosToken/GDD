f6fdd87cf34e0fd11548d5896893a71ae5dda5b5
Slavouz Corp <tmbwpva@protonmail.com>
2023-01-18 19:42:25 +0700

    Bug Fixes

---
+++ Assets/Scripts/KAKI.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KAKI : MonoBehaviour
+{
+    public float gravity = -100;
+    public Vector2 velocity;
+    public float maxAcceleration = 10;
+    public float acceleration = 10;
+    public float distance = 0;
+    public float jumpVelocity = 20;
+    public float maxVelocity = 100;
+    public float groundHeight = 8;
+    public bool isGrounded = false;
+    public bool isHoldingJump = false;
+    public float maxHoldJumpTime = 0.15f;
+    public float holdJumpTimer = 0.0f;
+    public float jumpGroundThreshold = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 post = transform.position;
+        float groundDistance = Mathf.Abs(post.y - groundHeight);
+        if (isGrounded || groundDistance <= jumpGroundThreshold)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                isGrounded = false;
+                velocity.y = jumpVelocity;
+                isHoldingJump = true;
+                holdJumpTimer = 0;
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            isHoldingJump = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 pos = transform.position;
+
+        if (!isGrounded)
+        {
+            if (isHoldingJump)
+            {
+                holdJumpTimer += Time.fixedDeltaTime;
+                if(holdJumpTimer >= maxHoldJumpTime) 
+                {
+                    isHoldingJump = false;
+                }
+            }
+
+            pos.y += velocity.y * Time.fixedDeltaTime;
+            if (!isHoldingJump) {
+                velocity.y += gravity * Time.fixedDeltaTime;
+            }            
+
+            if (pos.y <= groundHeight)
+            {
+                pos.y = groundHeight;
+                isGrounded = true;                
+            }
+        }
+
+        distance += velocity.x * Time.fixedDeltaTime;
+
+        if (isGrounded)
+        {
+            float velocityRation = velocity.x / maxVelocity;
+            acceleration = maxAcceleration * (1 - velocityRation);
+
+            velocity.x += acceleration * Time.fixedDeltaTime;
+            if (velocity.x >= maxVelocity)
+            {
+                velocity.x = maxVelocity;
+            }
+        }
+
+        transform.position = pos;
+
+    }
+}
